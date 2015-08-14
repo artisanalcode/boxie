378ffabef1325dad9d68d78bd6a4821192ca0946
Juan Garcia <jmgarcia@can2go.com>
2015-08-14 10:08:37 -0400

    Fix log file path issues.

--- config.sh
+++ config.sh
@@ -1,23 +1,27 @@
-# Virtual Box Configuration
-vbox_user="${USER}"
-vm_path="vms/"
-vm_temp="C:/Temp/"
-vm_mem="1024"
-vm_mem_xp="512"
-
+# Configuration
+#  |
+#  | Host machiche
+owner="${USER}"
+#  |--> Paths/Folders
+vms_path="vms/"
 log_path="logs/"
-
-nic_bridge="eth0"
-
-# Tools
 tools_path="$(pwd)/tools/"
-deuac_iso="deuac.iso"
 selenium_path="selenium_conf/"
+#  | --> Filenames
+deuac_iso="deuac.iso"
+chrome_exe="chrome.exe"
+firefox_exe="firefox.exe"
+nginx_folder="nginx-1.8.0"
+java_exe="jre-windows-i586.exe"
 ie_cache_reg="ie_disablecache.reg"
 ie_protectedmode_reg="ie_protectedmode.reg"
-java_exe="jre-windows-i586.exe"
-firefox_exe="firefox.exe"
-chrome_exe="chrome.exe"
 selenium_jar="selenium-server-standalone.jar"
-
-create_snapshot=False
+#  |
+#  | Guest machine
+#  |--> Config
+vm_mem="1024"
+vm_mem_xp="512"
+vm_nic_bridge="eth0"
+vm_create_snapshot=False
+#  |--> Paths
+temp_path="C:/Temp/"
